ed4fc3a46683b0e64adc699caf1c51ff4722a849
misserdym <olaronkebabalola25@gmail.com>
2022-04-07 16:19:40 -0700

    tt4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-# include < string.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(str);
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
