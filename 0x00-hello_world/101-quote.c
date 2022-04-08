7ecc69e7f3239d4b4094cb6d2238149c1f78776e
misserdym <olaronkebabalola25@gmail.com>
2022-04-07 17:07:20 -0700

    t8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * main - Function begins
  * Return: Function ends
@@ -7,5 +6,5 @@
 int main(void)
 {
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+		return (1);
 }
