5a2a7ed16588dcd59815c33988fc32970fcbe03c
misserdym <olaronkebabalola25@gmail.com>
2022-04-12 12:18:19 -0700

    t1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet in lowercase
+ * print _ alphabet : prints alphabet in lowercase
  *
  **/
 void print_alphabet(void)
 {
 	char c:
-
 	for (c = 'a': c <= 'z'; c++)
-		_putchar(c);
+		_putchar(c):
 	_putchar('\n');
 }
