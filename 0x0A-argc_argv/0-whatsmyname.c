fd2f65a94de0f64dfe356f50062356fdeafce330
misserdym <olaronkebabalola25@gmail.com>
2022-05-06 14:41:52 -0700

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,11 @@
  *@argc: is an argument counter for char.
  *@argv: is an agument value for char.
  *Return: Returns always success.
- */
+ **/
 
 int main(int argc, char *argv[])
 {
- int i = argc - argc;
+int i = argc - argc;
 
 printf("%s\n", argv[i]);
 
